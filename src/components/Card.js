6fa45ad1cce8c0b4958112a44d748ddb4dba64db
Flavio Bordomas <flaviocodigojs@gmail.com>
2022-07-25 22:12:45 -0300

    Implementación de Boostrap. Creación de carpeta assets, img. Importación de imágenes. Se muestra en Card aún sin estilos

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import lobo1 from '../assets/img/lobo1.jpg' 
 
 function Card() {
   return (
-    <div>Card</div>
+    <div className='card'>
+      <img src={lobo1} alt='' />
+      <div className='card-body'>
+        <div className='card-title'>Card</div>
+        <div className='card-text text-secondary'>
+          lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor
+          sit amet, consectetur adip
+        </div>
+      </div>
+    </div>
   )
 }
 
